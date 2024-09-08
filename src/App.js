6dce4afe6444a7e380edb2ebe447348e8a9cd93c
KESHAVBERG <kingkeshav7@gmail.com>
2024-09-08 17:56:53 +0530

    added the tabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,97 @@
-import './App.css';
-import Appbar from './components/Appbar'
+import "./App.css";
+import Appbar from "./components/Appbar";
+import { Box } from "@mui/material";
+import { useState } from "react";
+import { Tabs, Tab } from "@mui/material";
+
 function App() {
-  return <>
-    <Appbar />
-  </>
+  const [value, setValue] = useState("");
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setCurrentTabIndex(newValue)
+    
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Appbar />
+      <Box sx={{ width: "100%", mt:8,  display: 'flex', justifyContent: 'center' }}>
+        <Tabs
+        TabIndicatorProps={{ style: { display: "none" } }}
+        value={currentTabIndex}
+        onChange={handleChange}
+         variant="scrollable" 
+         scrollButtons="auto" 
+         sx={{
+           maxWidth: "100%",
+           overflowX: 'auto', 
+           display: 'flex',
+           justifyContent: 'center',
+         }}
+        >
+          <Tab label="General" 
+          className="tabs-ele" sx={{
+            borderRadius: '300px', 
+            bgcolor: 'white', 
+            mx: 1,
+            border: '1px solid grey',
+            "&.Mui-selected": {
+              bgcolor: "rgb(185, 157, 246)", 
+              color: "white",
+            },
+            '&:hover': {
+              bgcolor: 'rgb(185, 157, 246)', 
+              color:'white'
+            },
+          }} />
+          <Tab label="Sports" className="tabs-ele" sx={{
+            borderRadius: '300px', 
+            bgcolor: 'white', 
+            mx: 1,
+            border: '1px solid grey',
+            "&.Mui-selected": {
+              bgcolor: "rgb(185, 157, 246)",
+              color: "white",
+            },
+            '&:hover': {
+              bgcolor: 'rgb(185, 157, 246)', 
+              color:'white'
+            },
+          }}  />
+          <Tab label="Tech" className="tabs-ele"  sx={{
+            borderRadius: '300px', 
+            bgcolor: 'white', 
+            mx: 1,
+            border: '1px solid grey',
+            "&.Mui-selected": {
+              bgcolor: "rgb(185, 157, 246)",
+              color: "white",
+            },
+            '&:hover': {
+              bgcolor: 'rgb(185, 157, 246)', 
+              color:'white'
+            },
+          }}  />
+          <Tab label="Food" className="tabs-ele" sx={{
+            borderRadius: '300px', 
+            bgcolor: 'white', 
+            mx: 1,
+            border: '1px solid grey',
+            "&.Mui-selected": {
+              bgcolor: "rgb(185, 157, 246)", 
+              color: "white",
+            },
+            '&:hover': {
+              bgcolor: 'rgb(185, 157, 246)', 
+              color:'white'
+            },
+          }}  />
+        </Tabs>
+      </Box>
+    </>
+  );
 }
 
 export default App;
--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
-import { AppBar, Box } from '@mui/material'
-import './style.css'
-
+import { AppBar, Box, Typography } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import { motion } from "framer-motion";
 
 // https://arshadalisoomro.hashnode.dev/creating-a-navigation-bar-with-mui-appbar-component-in-nextjs
 const Appbar = () => {
   return (
-    <Box sx={{flexGrow:1}}>
-        <AppBar className='appbar' position='fixed'  sx={{ bgcolor: "white", zIndex: 0 }}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar className='appbar' position='fixed' sx={{ bgcolor: "white", zIndex: 0, pt: 2, px: { lg: 7, md: 7, sm: 2, sx: 2 }, display: "flex", flexDirection: "row" }}
         elevation={0}>
-            <p>Hello</p>
-        </AppBar>
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mx: 2,
+            display: 'flex',
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.2rem',
+            color: 'rgb(185, 157, 246)',
+            textDecoration: 'none',
+          }}>
+          ACONEWS
+        </Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <motion.div
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.5 },
+          }}
+        >
+          <SearchIcon sx={{ mx: 2, color: "black", }} />
+        </motion.div>
+      </AppBar>
     </Box>
   )
 }
